c49e2fa555a0d5181d6a45fb8732b0b3eec38422
Eaden McKee <mail@eaden.net>
2022-12-12 23:13:26 +1300

    try to workaround issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,23 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
-module ActiveSupport
-  class ConfigurationFile # :nodoc:
-    def parse(context: nil, **options)
-      source = render(context)
-      begin
-        YAML.load(source, aliases: true, **options) || {}
-      rescue ArgumentError
-        YAML.load(source, **options) || {}
-      end
-    rescue Psych::SyntaxError => error
-      raise "YAML syntax error occurred while parsing #{@content_path}. " \
-            "Please note that YAML must be consistently indented using spaces. Tabs are not allowed. " \
-            "Error: #{error.message}"
-    end
-  end
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
 
